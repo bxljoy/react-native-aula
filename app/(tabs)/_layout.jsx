0755ac8d38546d748560eb557f3566eae0112945
bxljoy <259500313@qq.com>
2024-08-22 13:53:17 +0200

    feat: add bottom navigation tabs for home, bookmark, create, and profile screens

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,61 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Tabs, Redirect } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "coral",
+          tabBarStyle: {
+            backgroundColor: "ivory",
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color }) => {
+              return <FontAwesome name="home" size={24} color={color} />;
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            headerShown: false,
+            tabBarIcon: ({ color }) => {
+              return <FontAwesome name="bookmark" size={24} color={color} />;
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ color }) => {
+              return <Ionicons name="add-circle" size={24} color={color} />;
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color }) => {
+              return <FontAwesome name="user" size={24} color={color} />;
+            },
+          }}
+        />
+      </Tabs>
+    </>
   );
 };
 
