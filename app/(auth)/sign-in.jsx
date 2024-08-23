05b0205a8215016fabd1cf06c7e599e3da4c43d7
bxljoy <259500313@qq.com>
2024-08-24 00:07:20 +0200

    feat: Add bottom navigation tabs for sign-in and sign-up screens

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,78 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import { useState } from "react";
+import { images } from "../../constants";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link } from "expo-router";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {
+    console.log(form);
+  };
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <View className="flex flex-row justify-start items-center w-full mt-[-10px]">
+            <Image
+              source={images.logoSmall}
+              className="w-[35px] h-[35px]"
+              resizeMode="contain"
+            />
+            <Text className="text-white text-4xl font-psemibold mt-2">
+              Aula
+            </Text>
+          </View>
+
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
+            Log in to Aula
+          </Text>
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="flex justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Signup
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, Text, View, Image } from "react-native";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full flex justify-start items-center h-full px-4">
+        <View className="w-full flex justify-center items-center min-h-[85vh] px-4">
           <View className="flex flex-row justify-center items-center w-full">
             <Image
               source={images.logoSmall}
